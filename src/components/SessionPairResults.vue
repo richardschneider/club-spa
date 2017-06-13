cc1eae8e43097821bde11db233d81008078c362b
Richard Schneider <makaretu@gmail.com>
2017-06-13 19:45:14 +1200

    feat(sessionPairResults): show the games

--- src/components/SessionPairResults.vue
+++ src/components/SessionPairResults.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="session-pair-results">
     <h3>{{ pair.title }}</h3>
+    <h4 v-if="pair.session">
+      <router-link :to="{ name: 'session-results', params: { id: pair.session.id }}">
+        {{ pair.session.title }}
+    </router-link>
+    <small>{{ pair.session.date }}</small>
+    </h4>
+    <games v-if="pair.games" :games="pair.games"></games>
   </div>
 </template>
 
@@ -8,12 +15,26 @@
 </style>
 
 <script>
+import Games from '@/components/Games'
 
 const query = `
 query sessionPair($id: ID!) {
   sessionPair(id: $id) {
     title
-}
+    session { id title date }
+    games {
+      lead
+      NS { pair { id shortTitle } matchpointsPercentage }
+      EW { pair { id shortTitle } matchpointsPercentage }
+      made
+      contract {
+        level
+        declaror
+        denomination
+        risk
+      }
+    }
+  }
 }`
 
 export default {
@@ -32,6 +53,7 @@ export default {
   computed: {
   },
   components: {
+    Games
   },
   methods: {
     fetch (pairId) {
