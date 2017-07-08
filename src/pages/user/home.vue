5b28e579053ed70cf568249dfc8cbaaf2ba72657
Richard Schneider <makaretu@gmail.com>
2017-07-08 17:01:20 +1200

    feat(user): session results WIP

--- src/pages/user/home.vue
+++ src/pages/user/home.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-    user home
     <h2>{{user.name}}</h2>
+    <h4>Results</h4>
+    <ul class="list-inline">
+      <li v-for="result in results">
+        <result :result="result"></result>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import auth from '@/auth'
+import Result from './result'
 
 export default {
   data () {
     return {
-      user: auth.user
+      user: auth.user,
+      results: [
+        { session: { id: 1, title: 'Winter Pairs', date: '2017-07-01' }, ranking: { rank: 1, score: 62.23, scale: 1 }, title: 'Joe Blogs / John Smith' },
+        { session: { id: 1, title: 'Winter Pairs', date: '2017-07-08' }, ranking: { rank: 5, score: 50.23, scale: 0.5 }, title: 'Joe Blogs / John Smith' },
+        { session: { id: 1, title: 'Winter Pairs', date: '2017-07-15' }, ranking: { rank: 2, score: 55.23, scale: 0.8 }, title: 'Joe Blogs / John Smith' },
+        { session: { id: 1, title: 'Winter Pairs', date: '2017-07-22' }, ranking: { rank: 3, score: 56.23, scale: 0.7 }, title: 'Joe Blogs / John Smith' }
+      ]
     }
+  },
+  components: {
+    Result
   }
 }
 </script>
---
+++ src/pages/user/result.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="result-card" @click="$router.push({ name: 'session-results', params: { id: result.session.id }})">
+    {{ result.session.title }}
+    <small>{{ result.session.date }}</small>
+    <span class="ranking">
+      <percent :value="result.ranking.score"></percent>%
+      <i class="fa fa-certificate" aria-hidden="true"></i>
+      {{ result.ranking.rank }}
+    </span>
+    <div>{{ result.title }}</div>
+    <star-rating
+      :inline="false"
+      inactive-color="white"
+      :item-size="18"
+      :read-only="true"
+      :rating="stars"
+      :show-rating="false"
+      :increment="0.01">
+    </star-rating>
+  </div>
+</template>
+
+<script>
+import percent from '@/components/percent'
+import {StarRating} from 'vue-rate-it'
+
+export default {
+  props: ['result'],
+  components: {
+    StarRating,
+    percent
+  },
+  computed: {
+    stars () {
+      return this.result.ranking.scale * 5
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+  .result-card {
+    border: 1px solid #d1d5da;
+    border-radius: 3px;
+    padding: 6px;
+    width: 24em;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+  .result-card .ranking {
+    float: right;
+  }
+  .result-card .fa-certificate {
+    color: limegreen;
+  }
+</style>
